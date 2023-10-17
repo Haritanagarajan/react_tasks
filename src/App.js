48bc2d709351e05faaaef7c3b5d28ae0289fc7db
ranitan <ranitanagarajan@gmail.com>
2023-10-17 17:30:23 +0530

    17/10/23

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserDetails from './Components/UserDetails/UserDetails';
 
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+      <Route exact path='/' element={<UserDetails/>}></Route>
+     </Routes>
+    </div>
+  );
+}
 
-class App extends React.Component {  
-   constructor(props) {  
-      super(props);  
-      this.state = {hello: "JavaTpoint"};  
-      this.changeState = this.changeState.bind(this)  
-   }    
-   render() {  
-      return (  
-         <div>  
-             <h1>ReactJS component's Lifecycle</h1>  
-             <h3>Hello {this.state.hello}</h3>  
-             <button onClick = {this.changeState}>Click Here!</button>          
-         </div>  
-      );  
-   }  
-   componentWillMount() {  
-      console.log('Component Will MOUNT!')  
-   }  
-   componentDidMount() {  
-      console.log('Component Did MOUNT!')  
-   }  
-   changeState(){  
-      this.setState({hello:"All!!- Its a great reactjs tutorial."});  
-   }  
-   componentWillReceiveProps(newProps) {      
-      console.log('Component Will Recieve Props!')  
-   }  
-   shouldComponentUpdate(newProps, newState) {  
-      return true;  
-   }  
-   componentWillUpdate(nextProps, nextState) {  
-      console.log('Component Will UPDATE!');  
-   }  
-   componentDidUpdate(prevProps, prevState) {  
-      console.log('Component Did UPDATE!')  
-   }  
-   componentWillUnmount() {  
-      console.log('Component Will UNMOUNT!')  
-   }  
-}  
-export default App;  
- 
+export default App;
